fe77423de0ccbce4fbf8863a0d15470fc9ef5e65
Araon39 <115640752+Araon39@users.noreply.github.com>
2025-01-28 16:31:30 +0200

    Add Coments+WEB

--- Assets/BuyShop.cs
+++ Assets/BuyShop.cs
@@ -5,33 +5,52 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BuyShop : MonoBehaviour
 {
+    // Количество магазинов
     public int shopAmount = 1;
+
+    // Текстовый элемент UI для отображения количества магазинов
     [SerializeField] private Text shopAmountText;
-    
+
+    // Ссылка на скрипт TAKEMoney
     private TAKEMoney takeMoneyScript;
-    
+
+    // Текстовый элемент UI для отображения стоимости покупки магазина
     [SerializeField] private Text costButtonShopBuy;
+
+    // Стоимость покупки магазина
     public int costShop = 50;
 
     private void Start()
     {
+        // Находим компонент TAKEMoney на объекте "GameManager"
         takeMoneyScript = GameObject.Find("GameManager").GetComponent<TAKEMoney>();
+
+        // Устанавливаем текст кнопки покупки магазина
         costButtonShopBuy.text = "Buy" + costShop.ToString() + " $";
     }
 
     public void Buy()
     {
+        // Проверяем, достаточно ли денег для покупки магазина
         if (takeMoneyScript.totalMoney >= costShop)
         {
+            // Уменьшаем общее количество денег на стоимость магазина
             takeMoneyScript.totalMoney -= costShop;
+
+            // Увеличиваем стоимость следующего магазина
             costShop *= 2;
-            
+
+            // Обновляем текст кнопки покупки магазина
             costButtonShopBuy.text = "Buy" + costShop.ToString() + " $";
+
+            // Обновляем текстовое поле с количеством денег
             takeMoneyScript.moneyText.text = takeMoneyScript.totalMoney.ToString();
-            
+
+            // Увеличиваем количество магазинов
             shopAmount += 1;
+
+            // Обновляем текстовое поле с количеством магазинов
             shopAmountText.text = shopAmount.ToString();
         }
-        
     }
 }
--- Assets/TAKEMoney.cs
+++ Assets/TAKEMoney.cs
@@ -5,36 +5,60 @@ using UnityEngine;
 using UnityEngine.UI;
 public class TAKEMoney : MonoBehaviour
 {
+    // Базовое количество денег, добавляемое каждый цикл
     [SerializeField] private int baseCount = 5;
+
+    // Общее количество денег
     public float totalMoney = 2;
+
+    // Текстовый элемент UI для отображения количества денег
     public Text moneyText;
+
+    // Ссылка на компонент BuyShop
     private BuyShop _buyShop;
 
+    // Слайдер для отображения прогресса
     [SerializeField] private Slider progressSlider;
+
+    // Время ожидания между циклами
     private float _storeTimer = 2f;
+
+    // Текущее время, прошедшее с начала цикла
     [SerializeField] private float _curentTime;
-    
+
     void Start()
     {
+        // Находим компонент BuyShop на объекте "NewStandPanel"
         _buyShop = GameObject.Find("NewStandPanel").GetComponent<BuyShop>();
+
+        // Запускаем корутину NewStand
         StartCoroutine(NewStand());
     }
 
     private void Update()
     {
+        // Увеличиваем текущее время на время, прошедшее с последнего кадра
         _curentTime += Time.deltaTime;
+
+        // Обновляем значение слайдера прогресса
         progressSlider.value = _curentTime / _storeTimer;
     }
 
     IEnumerator NewStand()
     {
+        // Ждем _storeTimer секунд
         yield return new WaitForSeconds(_storeTimer);
-        
-            totalMoney += baseCount * _buyShop.shopAmount;
-            moneyText.text = totalMoney.ToString();
-            
-            _curentTime = 0;
-        
+
+        // Увеличиваем общее количество денег на baseCount, умноженное на количество магазинов
+        totalMoney += baseCount * _buyShop.shopAmount;
+
+        // Обновляем текстовое поле с количеством денег
+        moneyText.text = totalMoney.ToString();
+
+        // Сбрасываем текущее время
+        _curentTime = 0;
+
+        // Перезапускаем корутину
         StartCoroutine(NewStand());
     }
 }
